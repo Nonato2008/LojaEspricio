a1e31e7cd9de7cadd8d6696bb6cf7f68b7a2c51d
unknown <silvabryan203@gmail.com>
2025-10-07 15:35:24 -0300

    atualizando

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -5,4 +5,8 @@ const {produtoController} = require("../controllers/produtoController");
 //GET /produtos -> Çista todos os produtos
 router.get("/produtos", produtoController.listarProdutos);
 
+// POST /produtos -> Cria um novo produto
+
+router.post("/produtos", produtoController.criarProduto);
+
 module.exports = {produtoRoutes: router};
